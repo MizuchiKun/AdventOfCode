76870840275dfc597ea61acf2da539b45164c309
MizuchiKun <til@jungbluths.de>
2025-11-23 11:04:03 +0100

    Finished AoC2024 Day2 Part1 in C.

--- AdventOfCode_C/AoC2024.c
+++ AdventOfCode_C/AoC2024.c
@@ -1,10 +1,11 @@
 ﻿// AoC2024.c : This file contains the 'main' function. Program execution begins and ends there.
 #include "AoC2024_Day1.h"
+#include "AoC2024_Day2.h"
 
 int main()
 {
     // Run the current day's code.
-    Day1();
+    Day2();
 
     return 0;
 }
